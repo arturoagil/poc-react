18dad04105e30d1bcadc38742ff893881bb47892
Arturo Zambrano <azambrano@inticousa.com>
2023-02-16 12:32:32 -0400

    Updates

---
+++ precache-manifest.40e4507a4d6bb079f58b8067a8ef415e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "142cedff1e9890394da48f79d747997d",
+    "url": "/poc-react/index.html"
+  },
+  {
+    "revision": "534851ef6f9ab517daab",
+    "url": "/poc-react/static/css/2.03989100.chunk.css"
+  },
+  {
+    "revision": "b5d395766c53dc864940",
+    "url": "/poc-react/static/css/main.76e35c4b.chunk.css"
+  },
+  {
+    "revision": "534851ef6f9ab517daab",
+    "url": "/poc-react/static/js/2.ddedddef.chunk.js"
+  },
+  {
+    "revision": "05d152d95f353b50aab1da3b4a1a642c",
+    "url": "/poc-react/static/js/2.ddedddef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5d395766c53dc864940",
+    "url": "/poc-react/static/js/main.f74f748d.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/poc-react/static/js/main.f74f748d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "699461f102bca7a1af6e",
+    "url": "/poc-react/static/js/runtime-main.0107df8f.js"
+  },
+  {
+    "revision": "361e8c85c185560d1223735e93ec6271",
+    "url": "/poc-react/static/media/dxicons.361e8c85.woff"
+  },
+  {
+    "revision": "9acd66044b85738abc90e5c630fefad2",
+    "url": "/poc-react/static/media/dxicons.9acd6604.ttf"
+  },
+  {
+    "revision": "a091c76c9b14a992ea5c4f414951565f",
+    "url": "/poc-react/static/media/dxicons.a091c76c.woff2"
+  }
+]);
